323b7dc72d65fd968de74ef948c1316d00b764fd
a.kroeger <a.kroeger@grimme.de>
2022-11-24 09:57:25 +0100

    test(unit): manufacturerService

--- VehicleManagement.Core.UnitTests/TestData/ManufacturerTestData.cs
+++ VehicleManagement.Core.UnitTests/TestData/ManufacturerTestData.cs
@@ -1,6 +1,38 @@
-﻿namespace VehicleManagement.Core.UnitTests.TestData
+﻿using System.Collections.Generic;
+
+using VehicleManagement.DataContracts.DataModels;
+
+namespace VehicleManagement.Core.UnitTests.TestData
 {
     public static class ManufacturerTestData
     {
+        public static IEnumerable<object[]> GetManufacturersTestData()
+        {
+            yield return new object[]
+            {
+                new List<Manufacturer>()
+                {
+                    new Manufacturer()
+                    {
+                        Id = "WAU",
+                        Name = "Audi"
+                    },
+                    new Manufacturer()
+                    {
+                        Id = "W0L",
+                        Name = "Opel"
+                    },
+                    new Manufacturer()
+                    {
+                        Id = "S1B"
+                    }
+                }
+            };
+
+            yield return new object[]
+            {
+                new List<Manufacturer>()
+            };
+        }
     }
 }
