91339cec3d031204db65b05189b8e526a3cc2e19
a.kroeger <a.kroeger@grimme.de>
2022-11-24 08:17:10 +0100

    test(integration): repositories

---
+++ VehicleManagement.DBAccess.IntegrationTests/DBFixtures/ManufacturerDBFixture.cs
@@ -0,0 +1,11 @@
+﻿namespace VehicleManagement.DBAccess.IntegrationTests.DBFixtures
+{
+    public class ManufacturerDBFixture : SharedDatabaseFixture
+    {
+        protected override void InitializeData()
+        {
+            InsertData(nameof(VehicleManagementContext.Manufacturer), "'WMI', 'Audi'");
+            InsertData(nameof(VehicleManagementContext.Manufacturer), "'W0L', 'Opel'");
+        }
+    }
+}
---
+++ VehicleManagement.DBAccess.IntegrationTests/DBFixtures/SharedDatabaseFixture.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace VehicleManagement.DBAccess.IntegrationTests.DBFixtures
+{
+    public abstract class SharedDatabaseFixture : IDisposable
+    {
+        private readonly SqliteConnection _connection;
+
+        protected SharedDatabaseFixture()
+        {
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+        }
+
+        public void Dispose()
+        {
+            _connection.Close();
+        }
+
+        public IVehicleManagementContext CreateContext()
+        {
+            var context = new VehicleManagementContext(
+                new DbContextOptionsBuilder<VehicleManagementContext>()
+                    .UseSqlite(_connection)
+                    .Options
+                );
+
+            context.Database.EnsureCreated();
+
+            InitializeData();
+
+            return context;
+        }
+
+        protected abstract void InitializeData();
+
+        protected void InsertData(string table, string valueCommand)
+        {
+            using var cmd = _connection.CreateCommand();
+
+            cmd.CommandText = $"INSERT INTO {table} VALUES ({valueCommand})";
+
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
