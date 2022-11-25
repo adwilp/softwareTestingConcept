04bfa4e3a9e33ed629ebf25d8343b892806d354d
a.kroeger <a.kroeger@grimme.de>
2022-11-25 13:12:08 +0100

    refactor(app): linted

--- VehicleManagement.App/src/app/app.component.spec.ts
+++ VehicleManagement.App/src/app/app.component.spec.ts
@@ -1,35 +1,37 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const fixture: ComponentFixture<AppComponent> =
+      TestBed.createComponent(AppComponent);
+    const app: AppComponent = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'VehicleManagementApp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const fixture: ComponentFixture<AppComponent> =
+      TestBed.createComponent(AppComponent);
+    const app: AppComponent = fixture.componentInstance;
     expect(app.title).toEqual('VehicleManagementApp');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture: ComponentFixture<AppComponent> =
+      TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('VehicleManagementApp app is running!');
+    const compiled: HTMLElement = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'VehicleManagementApp app is running!'
+    );
   });
 });
--- VehicleManagement.App/src/environments/environment.prod.ts
+++ VehicleManagement.App/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/typedef */
 export const environment = {
-  production: true
+  production: true,
 };
