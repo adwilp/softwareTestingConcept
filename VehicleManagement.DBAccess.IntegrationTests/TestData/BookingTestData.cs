06d1040bf4fc4987aab3362643d25fc962df190d
a.kroeger <a.kroeger@grimme.de>
2023-01-16 13:39:16 +0100

    test(booking): update Integration Tests

--- VehicleManagement.DBAccess.IntegrationTests/TestData/BookingTestData.cs
+++ VehicleManagement.DBAccess.IntegrationTests/TestData/BookingTestData.cs
@@ -53,6 +53,33 @@ namespace VehicleManagement.DBAccess.IntegrationTests.TestData
             };
         }
 
+        public static IEnumerable<object[]> GetFailUpdateTestData()
+        {
+            yield return new object[]
+            {
+                new Booking()
+                {
+                    Id = 5,
+                    Start = new System.DateTime(2022, 12, 15, 10, 43, 50),
+                    End = new System.DateTime(2022, 12, 16, 10, 0, 0),
+                    EmployeeNumber = "12345",
+                    FIN = "SB164ABN1PE082096"
+                }
+            };
+
+            yield return new object[]
+            {
+                new Booking()
+                {
+                    Id = 1,
+                    Start = new System.DateTime(2022, 12, 15, 10, 43, 50),
+                    End = new System.DateTime(2022, 12, 16, 10, 0, 0),
+                    EmployeeNumber = "12345",
+                    FIN = "DEF",
+                },
+            };
+        }
+
         public static IEnumerable<object[]> GetReloadReferencesTestData()
         {
             yield return new object[]
@@ -72,6 +99,49 @@ namespace VehicleManagement.DBAccess.IntegrationTests.TestData
                         Mileage = 12345.89,
                         ManufacturerId = "WMI"
                     }
+                },
+            };
+        }
+
+        public static IEnumerable<object[]> GetUpdateTestData()
+        {
+            yield return new object[]
+            {
+                new Booking()
+                {
+                    Id = 1,
+                    Start = new System.DateTime(2022, 12, 15, 10, 43, 50),
+                    End = new System.DateTime(2022, 12, 16, 10, 0, 0),
+                    EmployeeNumber = "12345",
+                    FIN = "SB164ABN1PE082986"
+                },
+                new Booking()
+                {
+                    Id = 1,
+                    Start = new System.DateTime(2022, 12, 15, 10, 43, 50),
+                    End = new System.DateTime(2022, 12, 16, 10, 0, 0),
+                    EmployeeNumber = "12345",
+                    FIN = "SB164ABN1PE082986",
+                },
+            };
+
+            yield return new object[]
+            {
+                new Booking()
+                {
+                    Id = 1,
+                    Start = new System.DateTime(2022, 12, 15, 10, 43, 50),
+                    End = new System.DateTime(2022, 12, 16, 10, 0, 0),
+                    EmployeeNumber = "12345",
+                    FIN = "SB164ABN1PE082096"
+                },
+                new Booking()
+                {
+                    Id = 1,
+                    Start = new System.DateTime(2022, 12, 15, 10, 43, 50),
+                    End = new System.DateTime(2022, 12, 16, 10, 0, 0),
+                    EmployeeNumber = "12345",
+                    FIN = "SB164ABN1PE082096",
                 },
             };
         }
